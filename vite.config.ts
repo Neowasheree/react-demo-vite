a09140c5403acb209fa7c85a41edf943680c9b47
Neowasheree <chlau629@gmail.com>
2025-06-25 10:35:50 +0200

    udpate

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tram_new.git/', // ✅ 注意要加斜杠
+  base: '/tram_new/', // 注意这里的斜杠：前后都需要
   plugins: [react()],
 });
