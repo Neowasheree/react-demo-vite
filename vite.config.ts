ed7419c1c37e37ca086a8f20841d310924877731
Neowasheree <chlau629@gmail.com>
2025-06-25 10:42:49 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tram_new/', // 注意这里的斜杠：前后都需要
+  base: '/tram_new/', // ← 这里是你的 GitHub 仓库名！前后都要加 /
   plugins: [react()],
 });
